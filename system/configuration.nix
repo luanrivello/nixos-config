205aedf66312e770408f5ec38d22cf8cba2dcfee
luanrivello <luanrivello@hotmail.com>
2023-01-08 15:31:41 -0300

    fix audio

--- system/configuration.nix
+++ system/configuration.nix
@@ -99,7 +99,7 @@
 
   #* Enable sound.
   sound.enable = true;
-  hardware.pulseaudio.enable = true;
+  hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
