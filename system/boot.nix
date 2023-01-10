d72595225d7d1f7b2274e22b832589a93f3e48f4
luanrivello <luanrivello@hotmail.com>
2023-01-10 20:51:19 -0300

    tartarus grub

--- system/boot.nix
+++ system/boot.nix
@@ -12,11 +12,17 @@
     device = "/dev/sda";
     useOSProber = true;
     #theme = pkgs.nixos-grub2-theme;
+#    theme = pkgs.fetchFromGitHub {
+#      owner = "shvchk";
+#      repo = "fallout-grub-theme";
+#      rev = "80734103d0b48d724f0928e8082b6755bd3b2078";
+#      sha256 = "sha256-7kvLfD6Nz4cEMrmCA9yq4enyqVyqiTkVZV5y4RyUatU=";
+#    };
     theme = pkgs.fetchFromGitHub {
-      owner = "shvchk";
-      repo = "fallout-grub-theme";
+      owner = "AllJavi";
+      repo = "tartarus-grub";
       rev = "80734103d0b48d724f0928e8082b6755bd3b2078";
-      sha256 = "sha256-7kvLfD6Nz4cEMrmCA9yq4enyqVyqiTkVZV5y4RyUatU=";
+      sha256 = "sha256-b116360a2a0991062a4d728cb005dfd309fbb82a";
     };
   };
 
