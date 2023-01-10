780a46e20c608f4bfe17b28ffa55c1190f42a58e
luanrivello <luanrivello@hotmail.com>
2023-01-10 19:40:18 -0300

    networking

--- system/configuration.nix
+++ system/configuration.nix
@@ -17,6 +17,7 @@
   #* Include the results of the hardware scan.
   imports = [ 
     ./hardware-configuration.nix 
+    ./networking.nix
     ./desktop.nix
     ./user.nix
   ];
@@ -50,18 +51,6 @@
     useOSProber = true;
     #TODO grub.theme = ;
   };
-
-  #* Define your hostname.
-  networking.hostName = "nexos"; 
-
-  #* Networking
-  networking.networkmanager.enable = true;  
-  #* Enables wireless support via wpa_supplicant.
-  #? networking.wireless.enable = true;  
-
-  #* Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   #* Set your time zone.
   time.timeZone = "Etc/GMT-3";
@@ -177,16 +166,5 @@
   #   enable = true;
   #   enableSSHSupport = true;
   # };
-
-  #* List services that you want to enable:
-
-  #* Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  #* Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  #! Or disable the firewall altogether.
-  # networking.firewall.enable = false;
 
 }
---
+++ system/networking.nix
@@ -0,0 +1,25 @@
+#* NETWORK CONFIGURATION
+{ config, pkgs, ... }:
+
+{
+  #* Define your hostname.
+  networking.hostName = "nexos"; 
+
+  #* Networking
+  networking.networkmanager.enable = true;  
+  #* Enables wireless support via wpa_supplicant.
+  #? networking.wireless.enable = true;  
+
+  #* Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  #* Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  #* Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  #! Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+}
