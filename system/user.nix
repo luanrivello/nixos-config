c61c2cf7ba3cd54778e2f0ce64d10aaffabae2b0
luanrivello <luanrivello@hotmail.com>
2023-01-17 18:56:54 -0300

    test

--- system/user.nix
+++ system/user.nix
@@ -45,7 +45,7 @@ in
 
       file = {
         #ALIASES
-        ".aliases".source = "${dotfiles}/enviroment/.aliases";
+        ".aliases".source = "./nixos-config/user/.dotfiles/enviroment/.aliases";
          
         #ALACRITTY
         #//".config/alacritty/alacritty.yml".source = ./dotfiles/alacritty.yml;
