e6cd2a2bcbf58d43ca5cd4793872a274f0de4a62
Luan Belem Rivello <58953615+luanrivello@users.noreply.github.com>
2023-05-01 19:20:13 +0000

    Add flake

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "My NixOS Flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = github:nix-community/home-manager;
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager }: 
+  let
+    system = "x86_64-linux";
+    hostname = "nexos";
+
+    pkgs = import nixpkgs {
+      inherit system;
+      config.allowUnfree = true;
+    };
+
+  in {
+    nixosConfigurations = {
+      ${hostname} = nixpkgs.lib.nixosSystem {
+          inherit pkgs system;
+          modules = [ 
+	    ./system/configuration.nix 
+	  ];
+      };
+    };
+  };
+}
