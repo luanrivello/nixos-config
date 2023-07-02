52e1b5615c01d6446024258a903b5f566cc59e51
luanrivello <luanrivello@hotmail.com>
2023-07-02 16:58:32 -0300

    printer

--- system/configuration.nix
+++ system/configuration.nix
@@ -67,9 +67,6 @@
     keyMap = "dvorak";
   };
 
-  #* Enable CUPS to print documents.
-  #? services.printing.enable = true;
-
   #* Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
