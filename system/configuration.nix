9fc584109d262ad73f46cff5bdb584ff207c3aa5
luanrivello <luanrivello@hotmail.com>
2023-07-02 17:02:52 -0300

    cleanup

--- system/configuration.nix
+++ system/configuration.nix
@@ -1,7 +1,3 @@
-#* Edit this configuration file to define what should be installed on
-#* your system.  Help is available in the configuration.nix(5) man page
-#* and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 
 #? nixos-install
@@ -67,9 +63,6 @@
     keyMap = "dvorak";
   };
 
-  #* Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
   #* Shell
   environment.binsh = "${pkgs.dash}/bin/dash";
   programs.zsh.enable = true;
@@ -100,8 +93,6 @@
   nixpkgs.config.allowUnfree = true;
   programs.steam.enable = true;
   environment.systemPackages = with pkgs; [
-
-    #*Terminal
     #alacritty
     #neovim
     htop
@@ -111,25 +102,5 @@
     file
     #stow
     killall
-
-    #*Internet
-    #firefox
-    #dolphin
-
-    #*Desktop
-    #xorg.xkill
-    #xorg.xrandr
-    #picom
-    #screenshoWiz
-
   ];
-
-  #! Some programs need SUID wrappers, can be configured further or are
-  #! started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
 }
