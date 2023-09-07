64743adb553e102ff100bdcbbff84a877d46c47b
luanrivello <luanrivello@hotmail.com>
2023-09-07 17:37:33 -0300

    update link

--- system/desktop/sddm-sugar-dark.nix
+++ system/desktop/sddm-sugar-dark.nix
@@ -1,8 +1,8 @@
 { pkgs }:
 let
   image = pkgs.fetchurl {
-    url = "https://github.com/luanrivello/.dotfiles/blob/main/interface/Interface/wallpapers/d_jod1lXsVCEtLhFFnzvrSqsgkj0P_6jAIYEa7UNXHI.jpg";
-    sha256 = "sha256-fl0spjpYezPvGZ6b4R/Mr18N7N3JdytCSwwu6mf4owQ";
+    url = "https://raw.githubusercontent.com/luanrivello/.dotfiles/main/interface/Interface/wallpapers/d_jod1lXsVCEtLhFFnzvrSqsgkj0P_6jAIYEa7UNXHI.jpg";
+    sha256 = "";
   };
 in
 pkgs.stdenv.mkDerivation {
