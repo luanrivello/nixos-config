cb8bf40ced65a9a7a113bdeccc970855d793f988
luanrivello <luanrivello@hotmail.com>
2023-09-07 20:11:49 -0300

    sha256

--- system/boot/tartarus-grub.nix
+++ system/boot/tartarus-grub.nix
@@ -6,7 +6,7 @@ pkgs.stdenv.mkDerivation {
     owner = "AllJavi";
     repo = "tartarus-grub";
     rev = "b116360a2a0991062a4d728cb005dfd309fbb82a";
-    sha256 = "0pa0w0124y07w1xjrr973nhak8p9p5j1rpaxzrlw2waczpdn1g3k";
+    sha256 = "sha256-/Pzr0R3zzOXUi2pAl8Lvg6aHTiwXTIrxQ1vscbEK/kU=";
   };
 
   installPhase = ''
