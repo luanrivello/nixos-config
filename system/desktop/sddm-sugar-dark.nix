b029ba7f1249baf28018fc8925c943b82b6a4bfa
luanrivello <luanrivello@hotmail.com>
2023-09-07 17:14:10 -0300

    sddm sugar dark

---
+++ system/desktop/sddm-sugar-dark.nix
@@ -0,0 +1,21 @@
+{ pkgs }:
+let
+  image = pkgs.fetchurl {
+    url = "https://github.com/luanrivello/.dotfiles/blob/main/interface/Interface/wallpapers/d_jod1lXsVCEtLhFFnzvrSqsgkj0P_6jAIYEa7UNXHI.jpg";
+    sha256 = "";
+  };
+in
+pkgs.stdenv.mkDerivation {
+  name = "sddm-sugar-dark";
+  src = pkgs.fetchFromGitHub {
+    owner = "MarianArlt";
+    repo = "sddm-sugar-dark";
+    rev = "ceb2c455663429be03ba62d9f898c571650ef7fe";
+    sha256 = "0153z1kylbhc9d12nyx9vpn0spxgrhgy36wy37pk6ysq7akaqlvy";
+  };
+
+  installPhase = ''
+  mkdir -p $out
+  cp -R ./* $out/
+  '';
+}
