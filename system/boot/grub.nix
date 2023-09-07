6d41a84cae2fc9ee613bce77534e3ac602356053
luanrivello <luanrivello@hotmail.com>
2023-09-07 19:31:31 -0300

    import tartarus

--- system/boot/grub.nix
+++ system/boot/grub.nix
@@ -4,11 +4,12 @@
     enable = true;
     device = "/dev/sda";
     useOSProber = true;
-    theme = pkgs.fetchFromGitHub {
-      owner = "shvchk";
-      repo = "fallout-grub-theme";
-      rev = "80734103d0b48d724f0928e8082b6755bd3b2078";
-      sha256 = "sha256-7kvLfD6Nz4cEMrmCA9yq4enyqVyqiTkVZV5y4RyUatU=";
-    };
+    theme = "${import ./tartarus-grub.nix { inherit pkgs; }}";
+    #theme = pkgs.fetchFromGitHub {
+    #  owner = "shvchk";
+    #  repo = "fallout-grub-theme";
+    #  rev = "80734103d0b48d724f0928e8082b6755bd3b2078";
+    #  sha256 = "sha256-7kvLfD6Nz4cEMrmCA9yq4enyqVyqiTkVZV5y4RyUatU=";
+    #};
   };
 }
