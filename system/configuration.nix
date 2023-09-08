c46327eea75cd16526d9b0638270b5b9d2752382
luanrivello <luanrivello@hotmail.com>
2023-09-08 09:52:05 -0300

    extra locale

--- system/configuration.nix
+++ system/configuration.nix
@@ -45,6 +45,9 @@
   time.timeZone = "Etc/GMT-3";
   i18n.defaultLocale = "en_US.UTF-8";
   i18n.extraLocaleSettings = {
+    LANGUAGE = "en_US.UTF-8";
+    LC_ALL = "pt_BR.UTF-8";
+    LC_ALL = "pt_BR.UTF-8";
     LC_ADDRESS = "pt_BR.UTF-8";
     LC_IDENTIFICATION = "pt_BR.UTF-8";
     LC_MEASUREMENT = "pt_BR.UTF-8";
