0e15ad6b4b4a3bf78cbb4b072fbb878706d6c6c9
luanrivello <luanrivello@hotmail.com>
2023-09-13 09:03:04 -0300

    x wallpaper and setup

--- system/desktop/bspwm.nix
+++ system/desktop/bspwm.nix
@@ -7,7 +7,10 @@
       xkbVariant = "dvorak";
       xkbOptions = "caps:swapescape";
 
+      services.xserver.desktopManager.wallpaper.mode = "fill";
+
       displayManager.defaultSession = "none+bspwm";
+      displayManager.setupCommands = "";
 
       windowManager.bspwm.enable = true;
     };
