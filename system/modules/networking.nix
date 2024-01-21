cfe96c511bcee1c8e7a518f1c7868126fba3a516
Luan Belem Rivello <luanrivello@hotmail.com>
2024-01-21 19:34:26 -0300

    Cleanup

--- system/modules/networking.nix
+++ system/modules/networking.nix
@@ -1,25 +1,21 @@
 { ... }:
 {
-  networking.hostName = "nexus"; 
+  networking = {
+    hostName = "nexus"; 
 
-  networking.networkmanager.enable = true;  
-  networking.nameservers = [ "9.9.9.9" "1.1.1.1" ]; 
+    wireless.enable = false;
+    networkmanager.enable = true;  
+    nameservers = [ "9.9.9.9" "1.1.1.1" ]; 
 
-  networking.firewall.enable = true;
-  networking.firewall.allowedTCPPorts = [ 8080 ];
+    firewall.enable = true;
+    firewall.allowedTCPPorts = [ 8080 ];
+    #firewall.allowedUDPPorts = [ ... ];
 
-  #* Open ports in the firewall
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # networking.firewall.enable = false; #! Or disable the firewall altogether.
+    #* Configure network proxy if necessary
+    #proxy.default = "http://user:password@proxy:port/";
+    #proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-  # networking.wireless.enable = true; #? Enables wireless support via wpa_supplicant.
-
-  #* Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
+  };
   #* Enable the OpenSSH daemon
-  # services.openssh.enable = true;
-
+  #services.openssh.enable = true;
 }
