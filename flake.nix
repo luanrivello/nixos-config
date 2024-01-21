8b9c3c7147f219444a9f81aac334761575209869
Luan Belem Rivello <luanrivello@hotmail.com>
2024-01-21 19:27:56 -0300

    Hyprland config

--- flake.nix
+++ flake.nix
@@ -10,6 +10,8 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    hyprland.url = "github:hyprwm/Hyprland";
+
   };
 
   outputs = { self, nixpkgs, ... }@inputs:
--- system/desktop/hyprlnd.nix
+++ system/desktop/hyprlnd.nix
@@ -1,8 +1,6 @@
-{ ... }:
-{
-  programs.sway.enable = true;
-  programs.hyprland = {
-    enable = true;
-    xwayland.enable = true;
-  };
+{ pkgs, lib, inputs, ... }:{
+  #programs.sway.enable = true;
+  #programs.hyprland.xwayland.enable = true;
+  programs.hyprland.enable = true;
+  programs.hyprland.package = inputs.hyprland.packages."${pkgs.system}".hyprland;
 }
