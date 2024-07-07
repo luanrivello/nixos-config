a33e4ad305bc22e2d403ae0868c38c3fc1153031
Luan Belem Rivello <luanrivello@hotmail.com>
2024-07-07 11:36:47 -0300

    Removed hyprland input

--- system/desktop/hyprland.nix
+++ system/desktop/hyprland.nix
@@ -2,7 +2,7 @@
   programs.sway.enable = true;
   programs.hyprland = {
     enable = true;
-    package = inputs.hyprland.packages."${pkgs.system}".hyprland;
+    #package = inputs.hyprland.packages."${pkgs.system}".hyprland;
 
     xwayland.enable = true;
   };
