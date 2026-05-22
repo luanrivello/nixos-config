e50ac3826d85510c852cb7100b512beb6f31e0df
Luan Belem Rivello <luanrivello@hotmail.com>
2026-05-22 16:28:20 -0300

    Closes 8080 port

--- system/modules/networking.nix
+++ system/modules/networking.nix
@@ -5,13 +5,14 @@
 
     networkmanager.enable = true;  
     networkmanager.dns = "none";  
+
     #ControlD Ads&Track, Quad9
     nameservers = [ "9.9.9.9" "149.112.112.112" "76.76.2.2" "76.76.10.2" 
                     "2620:fe::fe" "2620:fe::9" "2606:1a40::2" "2606:1a40:1:2"  
                   ]; 
 
     firewall.enable = true;
-    firewall.allowedTCPPorts = [ 8080 ];
+    #firewall.allowedTCPPorts = [ 8080 ];
     #firewall.allowedUDPPorts = [ ... ];
 
     #* Configure network proxy if necessary
